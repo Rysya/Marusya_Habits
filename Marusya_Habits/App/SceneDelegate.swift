bc66d412bc5cf8ac8d64c709640d1cc70d5bf49b
Rysya <pbl6a@bk.ru>
2026-02-03 21:32:04 +0300

    delete commetaries

--- Marusya_Habits/App/SceneDelegate.swift
+++ Marusya_Habits/App/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  Marusya_Habits
-//
-//  Created by Мария Александрова on 11.01.2026.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -38,12 +31,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
       
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        
-        // Создаем окно
+
         window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
-
         showLaunchScreen()
     }
     
@@ -52,59 +42,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window?.rootViewController = launchScreenViewController
             window?.makeKeyAndVisible()
             
-            // Имитируем загрузку данных (3 секунды)
             DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
                 self.showMainApp()
             }
         }
     
-    // Перейти к основному приложению
     private func showMainApp() {
         
             let habitsViewController = HabitsViewController()
             let navigationController = UINavigationController(rootViewController: habitsViewController)
-            // Анимация перехода
-            UIView.transition(with: window!, duration: 0.5, options: .transitionCrossDissolve, animations: {
-                self.window?.rootViewController = navigationController
-
-            }, completion: { _ in
-                
-                self.launchScreenViewController = nil // Освобождаем память
-            })
         
+            UIView.transition(with: window!,
+                              duration: 0.5,
+                              options: .transitionCrossDissolve,
+                              animations: {
+                self.window?.rootViewController = navigationController
+            }, completion: { _ in
+                self.launchScreenViewController = nil
+            })
         
         self.window?.rootViewController = createTabBar()
         self.window?.makeKeyAndVisible()
     }
-    
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
-
